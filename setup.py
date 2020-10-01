4acd0695e3d054a9bfb1b71a98579ff675c5d595
Ben Cail <benjamin_cail@brown.edu>
2020-10-01 11:13:36 -0400

    loosen dep requirements

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(name='mods_generator',
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'bdrxml @ https://github.com/Brown-University-Library/bdrxml/archive/v0.9.zip#sha1=9eeff5ed1435dac16795d54680112e15ba3bb485',
-        'xlrd==1.1.0',
+        'bdrxml',
+        'xlrd',
     ]
 )
 
